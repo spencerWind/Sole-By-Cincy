c34257919a4642b1d9670c0ec58d01d3ba1881ff
spencerWind <swind03@gmail.com>
2022-12-27 02:55:13 -0500

    Created toggle for hiding and showing forms

---
+++ sbc-customize-page/src/index.js
@@ -0,0 +1,34 @@
+var two_color = document.getElementById('two-color');
+var three_color = document.getElementById('three-color');
+var custom_config = document.getElementById('custom-config');
+
+
+function twoColorToggle(){
+    if(two_color.classList.contains('hidden')){
+        two_color.classList.remove('hidden');
+    }
+
+    three_color.classList.add('hidden');
+    custom_config.classList.add('hidden');
+
+}
+
+function threeColorToggle(){
+    if(three_color.classList.contains('hidden')){
+        three_color.classList.remove('hidden');
+    }
+
+    two_color.classList.add('hidden');
+    custom_config.classList.add('hidden');
+
+}
+
+function customConfigToggle(){
+    if(custom_config.classList.contains('hidden')){
+        custom_config.classList.remove('hidden');
+    }
+    
+    two_color.classList.add('hidden');
+    three_color.classList.add('hidden');
+
+}
